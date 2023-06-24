ebd9cbee51c5c02c5646e889225c3bac68aa7d8d
Ladislav Dono <ladislav.dono@student.tuke.sk>
2023-06-24 15:04:33 +0200

    whereisthis

---
+++ src/products.js
@@ -0,0 +1,17 @@
+import product1 from './assets/1.png';
+import product2 from './assets/2.png';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Iphone",
+        price: 999.9,
+        productImage: product1, 
+    },
+    {
+        id: 2,
+        productName: "Macbook",
+        price: 1999.9,
+        productImage: product2, 
+    },
+];
