f9a7056ede04ac06046d66eade48f66265ed268d
Ladislav Dono <ladislav.dono@student.tuke.sk>
2023-07-03 22:07:35 +0200

    update

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { PRODUCTS } from '../../products'
+import { ShopContext } from '../../context/shop-context'
 
 export const Cart = () => {
+
+  const { cartItems } = useContext(ShopContext);
+
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className='cartItems'>
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} /> //vytvorit CartItem komponent
+          }
+        })}
+      </div>
+    </div>
   )
 }
