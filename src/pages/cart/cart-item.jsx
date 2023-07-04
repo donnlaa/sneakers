7f885e1a9b769f200d1a2a0783727037b75a7479
Ladislav Dono <ladislav.dono@student.tuke.sk>
2023-07-04 23:02:31 +0200

    adding more functions to cart

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../../context/shop-context';
 
 export const CartItem = (props) => {
     const { id, productName, price, productImage } = props.data;
+    const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
+
     return (
         <div className='cartItem'>
             <img src={productImage} />
@@ -12,6 +15,11 @@ export const CartItem = (props) => {
                 <p>
                     ${price}
                 </p>
+                <div className='countHandler'>
+                    <button onClick={() => removeFromCart(id)}> - </button>
+                    <input value={cartItems[id]} />
+                    <button onClick={() => addToCart(id)}> + </button>
+                </div>
             </div>
         </div>
     )
