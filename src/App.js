e07e494aa5fc6fa08196a6c7c6cb7ebec97c0e2a
Ladislav Dono <ladislavdono1@gmail.com>
2023-07-06 15:33:59 +0200

    adding product detail page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Navbar } from "./components/navbar";
 import { Shop } from './pages/shop/shop';
 import { Cart } from './pages/cart/cart';
 import { ShopContextProvider } from './context/shop-context';
+import { ProductDetail } from './pages/shop/product-detail';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Shop />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/product-detail" element={<ProductDetail />} />
           </Routes>
         </Router>
       </ShopContextProvider>
--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { ShopContext } from '../../context/shop-context';
+import { Link } from 'react-router-dom';
 
 export const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
@@ -8,7 +9,7 @@ export const Product = (props) => {
   const cartItemAmount = cartItems[id];
   return (
     <div className='product'>
-      <img src={productImage} />
+      <Link to="/product-detail"><img src={productImage} /></Link>
       <div className='description'>
         <p>
           <b>{productName}</b>
